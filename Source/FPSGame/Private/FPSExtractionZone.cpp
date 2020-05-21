31e4cb7092ec274bb0f58fbcd3594aa7f3015902
Fabricio Manrique <fabricio.manrique@bechtle.com>
2020-05-21 17:59:24 -0300

    Added a mission complete state

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -3,12 +3,15 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
 {
 	Size = 200.0f;
- 	OverlapBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapBoxComponent"));
+	OverlapBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapBoxComponent"));
 	RootComponent = OverlapBoxComponent;
 
 	OverlapBoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
@@ -23,7 +26,25 @@ AFPSExtractionZone::AFPSExtractionZone()
 	DecalComponent->DecalSize = FVector(Size);
 }
 
-void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
 {
+	AFPSCharacter *character = Cast<AFPSCharacter>(OtherActor);
 
+	if (character == nullptr)
+		return;
+
+	if (character->bIsCarryingObjective)
+	{
+		AFPSGameMode *GameMode = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+
+		if (GameMode)
+		{
+			GameMode->CompleteMission(character);
+		}
+	}
+	else
+	{
+		if(ObjectiveMissingSound != nullptr)
+			UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
 }
